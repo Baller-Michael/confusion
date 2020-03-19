403fa01402ba0f05283651ebe1c4235177061ff7
Michael <mihalu41998@gmail.com>
2020-03-19 13:14:41 +0200

    react route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Main from '../src/components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
 import { DISHES } from './shared/dishes';
 import './App.css'
 
@@ -11,9 +12,13 @@ class App extends React.Component {
     };
   };
   render() {
-    return <div>
-      <Main />
-    </div>
+    return (
+      <BrowserRouter>
+        <div>
+          <Main />
+        </div>
+      </BrowserRouter>
+    )
   }
 };
 
